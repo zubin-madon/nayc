d215ee633433df44b67101f957a32adbb201c9d0
zubin-madon <madon.zubin@gmail.com>
2022-01-16 04:20:53 +0530

    first

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,27 +3,28 @@ import './card.css'
 
 function Card () {
         return (
-    <div className='card-page mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-        <div className="wrapper">
-  <div className="card">
+    <div className='card-page px-4 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+
+    <div className="mx-3 wrapper">
+    <div className="card">
     <h1>
       <span className="enclosed">Secondary Listing Post Mint</span>ME/ DE
     </h1>
   </div>
-</div>
+</div>&nbsp;&nbsp;
 
-<div className="wrapper">
+<div className="mx-3 wrapper">
   <div className="card1">
     <h1>
       <span className="enclosed">Future</span>Community/<br/>DAO<br/>Gamification<br/>
     </h1>
   </div>
-</div>
+</div>&nbsp;&nbsp;
 
-<div className="wrapper">
+<div className="mx-3 wrapper">
   <div className="card2">
     <h1>
-      <span className="enclosed">Perks</span><br/>Neural Art Airdrops<br/> Project Collabs
+      <span className="enclosed">Perks</span><br/>Neural Art Airdrops<br/>Project Collabs
     </h1>
   </div>
 </div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/*.{html,js}',
+    './index.html',
+    './public/index.html',
+    './src/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
